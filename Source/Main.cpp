80e067a4015ffd68ce7d8d7f753076d0401d0b2b
Tate House <tatemonroehouse@gmail.com>
2024-02-04 21:27:49 -0500

    Feature: Write PPM image

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,4 +1,7 @@
 #include <cstddef>
+#include <fstream>
+#include <filesystem>
+#include <vector>
 
 unsigned int packColor(const std::byte red,
                        const std::byte green,
@@ -19,6 +22,28 @@ void unpackColor(const unsigned int color,
 	green = static_cast<std::byte>((color >> 8) & 255);
 	blue = static_cast<std::byte>((color >> 16) & 255);
 	alpha = static_cast<std::byte>((color >> 24) & 255);
+}
+
+void writeImage(const std::filesystem::path& path,
+                const std::vector<unsigned int>& image,
+                const unsigned int imageWidth,
+                const unsigned int imageHeight) {
+	if (image.size() != imageWidth * imageHeight) {
+		throw std::invalid_argument("Image size does not match width and height");
+	}
+	
+	std::ofstream fileStream {path, std::ios::out | std::ios::binary};
+	fileStream << "P6\n" << imageWidth << " " << imageHeight << "\n255\n";
+	for (std::size_t pixelIndex {0}; pixelIndex < image.size(); ++pixelIndex) {
+		std::byte red {};
+		std::byte green {};
+		std::byte blue {};
+		std::byte alpha {};
+		unpackColor(image[pixelIndex], red, green, blue, alpha);
+		fileStream << static_cast<char>(red) << static_cast<char>(green) << static_cast<char>(blue);
+	}
+	
+	fileStream.close();
 }
 
 int main(int argc, char* argv[]) {
