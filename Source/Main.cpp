520f64d51f64f857c3dc8cc10a1477ba07eb32f2
Tate House <tatemonroehouse@gmail.com>
2024-02-04 21:13:35 -0500

    Feature: Color packing and unpacking

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,3 +1,21 @@
+unsigned int packColor(const unsigned char red,
+                       const unsigned char green,
+                       const unsigned char blue,
+                       const unsigned char alpha = 255) {
+	return (alpha << 24) + (blue << 16) + (green << 8) + red;
+}
+
+void unpackColor(const unsigned int color,
+                 unsigned char& red,
+                 unsigned char& green,
+                 unsigned char& blue,
+                 unsigned char& alpha) {
+	red = (color >> 0) & 255;
+	green = (color >> 8) & 255;
+	blue = (color >> 16) & 255;
+	alpha = (color >> 24) & 255;
+}
+
 int main(int argc, char* argv[]) {
 	return 0;
 }
