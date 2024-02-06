f74b8f85245ab2891cd84f623b41e97d1cf1d799
Tate House <tatemonroehouse@gmail.com>
2024-02-05 20:50:55 -0500

    Feature: Load and display texture
    This completes "Step 9: Loading the textures".

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,7 +1,10 @@
+#include "STBImage.hpp"
+
 #include <array>
 #include <cstddef>
 #include <fstream>
 #include <filesystem>
+#include <iostream>
 #include <numbers>
 #include <random>
 #include <sstream>
@@ -77,6 +80,47 @@ void drawRectangle(std::vector<unsigned int>& image,
 	}
 }
 
+void loadTexture(const std::filesystem::path& path,
+                 std::vector<unsigned int>& texture,
+                 unsigned int& textureSize,
+                 unsigned int& textureCount) {
+	auto channelCount {-1};
+	auto width {0};
+	auto height {0};
+	auto* pixelMap {stbi_load(path.string().c_str(), &width, &height, &channelCount, 0)};
+	
+	if (!pixelMap) {
+		throw std::invalid_argument {"Failed to load texture"};
+	}
+	
+	constexpr auto expectedChannelCount {4};
+	if (channelCount != expectedChannelCount) {
+		stbi_image_free(pixelMap);
+		throw std::invalid_argument {"Texture must have 4 channels"};
+	}
+	
+	textureCount = width / height;
+	textureSize = width / textureCount;
+	if (width != height * textureCount) {
+		stbi_image_free(pixelMap);
+		throw std::invalid_argument {"Texture file must contain N square textures packed horizontally"};
+	}
+	
+	texture = std::vector<unsigned int>(width * height);
+	for (std::size_t rowIndex {0}; rowIndex < height; ++rowIndex) {
+		for (std::size_t columnIndex {0}; columnIndex < width; ++columnIndex) {
+			const auto textureIndex {rowIndex * width + columnIndex};
+			const auto red {static_cast<std::byte>(pixelMap[textureIndex * expectedChannelCount + 0])};
+			const auto green {static_cast<std::byte>(pixelMap[textureIndex * expectedChannelCount + 1])};
+			const auto blue {static_cast<std::byte>(pixelMap[textureIndex * expectedChannelCount + 2])};
+			const auto alpha {static_cast<std::byte>(pixelMap[textureIndex * expectedChannelCount + 3])};
+			texture[textureIndex] = packColor(red, green, blue, alpha);
+		}
+	}
+	
+	stbi_image_free(pixelMap);
+}
+
 [[maybe_unused]] void drawGradientBackground(const unsigned int imageWidth,
                                              const unsigned int imageHeight,
                                              std::vector<unsigned int>& image) {
@@ -133,6 +177,22 @@ int main(int argc, char* argv[]) {
 		const auto green {static_cast<std::byte>(randomEngine() % 255)};
 		const auto blue {static_cast<std::byte>(randomEngine() % 255)};
 		colors[colorIndex] = packColor(red, green, blue);
+	}
+	
+	std::vector<unsigned int> wallTextures {};
+	unsigned int textureSize {};
+	unsigned int textureCount {};
+	try {
+		loadTexture(std::filesystem::path {"Resources/Textures/WallTextures.png"},
+		            wallTextures,
+		            textureSize,
+		            textureCount);
+	} catch (const std::invalid_argument& exception) {
+		std::cerr << exception.what() << std::endl;
+		return -1;
+	} catch (const std::filesystem::filesystem_error& exception) {
+		std::cerr << exception.what() << std::endl;
+		return -1;
 	}
 	
 	for (std::size_t frameIndex {0}; frameIndex < 360; ++frameIndex) {
@@ -195,6 +255,15 @@ int main(int argc, char* argv[]) {
 					              colors[colorIndex]);
 					break;
 				}
+			}
+		}
+		
+		const auto textureId {2};
+		for (std::size_t textureCoordinateX {0}; textureCoordinateX < textureSize; ++textureCoordinateX) {
+			for (std::size_t textureCoordinateY {0}; textureCoordinateY < textureSize; ++textureCoordinateY) {
+				const auto imageIndex {textureCoordinateX + textureCoordinateY * imageWidth};
+				const auto textureIndex {textureCoordinateX + textureId * textureSize + textureCoordinateY * textureSize * textureCount};
+				image[imageIndex] = wallTextures[textureIndex];
 			}
 		}
 		
